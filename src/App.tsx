862d6b28f066b17e060a82259da7aa9e5565028a
Amit Kushwaha <amitcoding56@gmail.com>
2025-07-22 08:58:35 +0530

    Initial Setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-5xl font-bold text-red-500">Hello</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
